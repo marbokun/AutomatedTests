917fb68a40fdabfda60bc886a59dec33f1084887
marbokun <marbokun2@gmail.com>
2024-05-05 16:06:00 +0900

    reinstate setproperty changes

--- src/test/java/seleniumTests/launchGmail.java
+++ src/test/java/seleniumTests/launchGmail.java
@@ -7,9 +7,9 @@ public class launchGmail {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		String path = "C:\\MyDevelopment\\geckodriver.exe";
-//		path = path.replace("\\", "/");
-//		System.setProperty("webdriver.firefox.driver", path);
+		String path = "C:\\MyDevelopment\\geckodriver.exe";
+		path = path.replace("\\", "/");
+		System.setProperty("webdriver.firefox.driver", path);
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.gmail.com/");
 		System.out.println(driver.getTitle());
